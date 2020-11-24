d744d4e0fcd5a77b177ef868332c1abb9cad0a5e
Niklas Merz <nmerz@gedys-intraware.de>
2020-11-24 17:31:11 +0100

    Basic proxy working

--- www/WebviewProxy.js
+++ www/WebviewProxy.js
@@ -23,4 +23,17 @@ WebviewProxy.prototype.setHostname = function (name, successCallback, errorCallb
   );
 };
 
+WebviewProxy.prototype.convertProxyUrl = function (path) {
+  if (!path || !window.CDV_ASSETS_URL) {
+      return path;
+  }
+  if (path.startsWith('http://')) {
+      return window.CDV_ASSETS_URL + '/_http_proxy_' + encodeURIComponent(path.replace('http://', ''));
+  }
+  if (path.startsWith('https://')) {
+      return window.CDV_ASSETS_URL + '/_https_proxy_' + encodeURIComponent(path.replace('https://', ''));
+  }
+  return path;
+}
+
 module.exports = new WebviewProxy();
